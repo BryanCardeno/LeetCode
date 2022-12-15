6881e8d1f1c470c825ecfd6b7c4763aba9772fd8
BryanCardeno <bryan_ca123@yahoo.com>
2022-12-14 21:27:47 -0800

    C++ files added

---
+++ Cpp/Easy/Easy/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>;
+#include <stdlib.h>;
+#include "Header.h";
+
+int main() {
+
+	using std::cout;
+	using std::endl;
+
+		cout << mySqrt(144) << endl;
+	
+}
+
---
+++ Cpp/Easy/Easy/sqrt.cpp
@@ -0,0 +1,37 @@
+#include <time.h>;
+#include <stdlib.h>
+
+int mySqrt(int x) {
+	srand(time(NULL));
+
+	int current{ rand() % 46340 };
+	int min{ 0 };
+	int minPlusOne{ min + 1 };
+	int max{ 46340 };
+	int result{ current };
+
+	if (max * max == x || x > max * max) {
+		return max;
+	}
+	else {
+		for (int i = 0; i < 31; i++) {
+			if (x < current * current) {
+				{
+					max = current;
+				}
+			}
+			else {
+				min = current;
+				minPlusOne = min + 1;
+			}
+
+			if (x >= min * min && x < minPlusOne * minPlusOne) {
+				result = min;
+				break;
+			}
+			current = (min + max) / 2;
+		}
+		
+	}
+	return result;
+}
